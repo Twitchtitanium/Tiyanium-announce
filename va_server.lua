844f6821059fdab6b85f1d24c19b537130c51c66
Twitchtitanium <57332409+Twitchtitanium@users.noreply.github.com>
2019-11-04 19:34:58 +0100

    Add files via upload

---
+++ va_server.lua
@@ -0,0 +1,9 @@
+--Titanium-announce
+-----[ CODE, DON'T TOUCH THIS ]-------------------------------------------
+RegisterServerEvent('va:getPlayerIdentifiers')
+AddEventHandler('va:getPlayerIdentifiers', function()
+    if GetPlayerIdentifiers(source) ~= nil then
+        TriggerClientEvent('va:setPlayerIdentifiers', source, GetPlayerIdentifiers(source))
+    end
+end)
+--------------------------------------------------------------------------
